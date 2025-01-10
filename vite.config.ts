fe69b22308323b0782ff127f2be96c0677b99fb7
Savazh <your_email@example.com>
2025-01-10 21:13:10 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/token/fuel-network', // Remove this line to use root path
   build: {
     chunkSizeWarningLimit: 1000,
     outDir: 'dist',
